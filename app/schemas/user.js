afc8d07d16ad62a8ecd8dd11c8006f1e542dd75c
miaodx <miaodx@hotmail.com>
2016-12-08 22:39:48 +0800

    miaodx; get the userRole.

--- app/schemas/user.js
+++ app/schemas/user.js
@@ -77,7 +77,7 @@ UserSchema.statics = {
   //查询单条目录
   findById: function(id, cb){
     return this
-      .findOne({_id: id})
+      .findOne({_id: id}).populate('userRole')
       .exec(cb);
   }
 };
